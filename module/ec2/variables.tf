ecf4f910bb78ee5d59e9f7310c9f2be74bc3cee8
balasubramaniyand <132192864+balasubramaniyand@users.noreply.github.com>
2024-03-10 11:24:18 +0530

    Update variables.tf

--- module/ec2/variables.tf
+++ module/ec2/variables.tf
@@ -1,23 +1,35 @@
 variable "aws_region" {
-  type = string
+  type        = string
   description = "The AWS region to deploy the resources in."
-  default = "ap-southeast-1"  # Match the region in your main.tf configuration
+  default     = "ap-southeast-1"  # Match the region in your main.tf configuration
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "The CIDR block for the VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet1_cidr_block" {
+  type        = string
+  description = "The CIDR block for the public subnet."
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  type        = string
+  description = "The availability zone to use for the subnet."
+  default     = "ap-southeast-1a"  # Update this to a valid AZ for your chosen region
 }
 
 variable "instance_type" {
-  type = string
+  type        = string
   description = "The instance type for the EC2 instance."
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "ami_id" {
-  type = string
+  type        = string
   description = "The AMI ID to use for the EC2 instance."
-  default = "ami-0eb4694aa6f249c52"
-}
-
-variable "security" {
-  type = string
-  description = "The name of the security group for the EC2 instance."
-  default = "web-server-sg"
+  default     = "ami-0eb4694aa6f249c52"
 }
