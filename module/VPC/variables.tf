0729d195ef6f5d6644de6226041d70a7d6a88460
balasubramaniyand <132192864+balasubramaniyand@users.noreply.github.com>
2024-03-10 09:36:45 +0530

    Update variables.tf

--- module/VPC/variables.tf
+++ module/VPC/variables.tf
@@ -9,7 +9,12 @@ variable "vpc_cidr_block" {
     default = "10.0.0.0/16"
   
 }
-variable "subnet_cidr_block" {
+variable "public_subnet1_cidr_block" {
+    type = string
+    default = "10.0.1.0/24"
+  
+}
+variable "public_subnet2_cidr_block" {
     type = string
     default = "10.0.1.0/24"
   
