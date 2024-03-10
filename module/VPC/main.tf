0ddb62704d4285d1fc2749f08b7036b633ef6093
balasubramaniyand <132192864+balasubramaniyand@users.noreply.github.com>
2024-03-10 09:35:33 +0530

    Update main.tf

--- module/VPC/main.tf
+++ module/VPC/main.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "public_subnet1" {
   vpc_id          = aws_vpc.my_vpc.id
   cidr_block      = var.public_subnet1_cidr_block
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-southeast-1a"
 
   tags = {
     Name = "public-subnet1"
@@ -24,7 +24,7 @@ resource "aws_subnet" "public_subnet1" {
 resource "aws_subnet" "public_subnet2" {
   vpc_id          = aws_vpc.my_vpc.id
   cidr_block      = var.public_subnet2_cidr_block
-  availability_zone = "ap-south-1b"  # Use a different availability zone for redundancy
+  availability_zone = "ap-southeast-1b"  # Use a different availability zone for redundancy
 
   tags = {
     Name = "public-subnet2"
